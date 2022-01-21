2c7f56119b5d8a9bcc4c4fb74c49c69c42261258
V Verma <vverm2@illinois.edu>
2022-01-21 17:54:08 -0600

    started lab1

--- lab1/lab1.asm
+++ lab1/lab1.asm
@@ -1,3 +1,8 @@
 .ORIG x3000
-
 ; write your code here
+        AND R1, R1, #0 ;initializing digit counter
+        ADD R1, R1, #4 ;make digit counter equal to 4
+        BRz Done
+        
+
+Done    HALT ;End of the program
