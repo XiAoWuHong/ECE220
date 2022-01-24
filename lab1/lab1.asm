38da4c68eaefa9e87223ee77bffc22e1aac5ebc5
V Verma <vverm2@illinois.edu>
2022-01-24 15:14:31 -0600

    lab1 working on it still

--- lab1/lab1.asm
+++ lab1/lab1.asm
@@ -1,8 +1,8 @@
 .ORIG x3000
 ; write your code here
-        AND R1, R1, #0 ;initializing digit counter
-        ADD R1, R1, #4 ;make digit counter equal to 4
-Loop2   ADD R1, R1, #0 ;Setcc
+        AND R6, R6, #0 ;initializing digit counter
+        ADD R6, R6, #4 ;make digit counter equal to 4
+Loop2   ADD R6, R6, #0 ;Setcc
         BRz Done
         AND R2, R2, #0
         ADD R2, R2, #4 ;initialize bit counter to 4
@@ -20,6 +20,6 @@ ZeroAdd ADD R3, R3, #9
 OutCom  AND R0, R0, #0 ; initialize and clear R0
         ADD R0, R3, R0 ; Put character in R3 into R0
         OUT            ; Print Character in R0
-
+        
 
 Done    HALT ;End of the program
