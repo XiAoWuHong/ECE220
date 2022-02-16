5077d9cf34ce0f3fc2f1cb75e5e515c7193b6e48
V Verma <vverm2@illinois.edu>
2022-02-15 22:02:03 -0600

    :wq

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,14 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define STOP 0
 
 int main()
 {
-  int row;
+  int row;          //represents n in the pascal's triangle equation
+  int i;            //represents i in the pascal's triangle equation given to us
+  unsigned result;  //made to store the result of the pascal's triangle equation
+  unsigned productloop;  
+  unsigned numerator;
+  unsigned denominator;
 
   printf("Enter a row index: ");
-  scanf("%d",&row);
+  scanf("%d", &row);
 
   // Write your code here
+  
+  //this loop gets us the correct amounnt of resultants for pascals triangle
+for (i = 0; i <= row; i++)
+{
+
+
+//initializes the numerator and denominator so we don't explode during the math
+  numerator = 1;
+  denominator = 1;
+
+//finds the numerator for the pascal's triangle equation given
+  for (productloop = i; productloop > 0; productloop--) 
+  {
+    numerator = numerator * (row + 1 - productloop); //for every 
+  }
+
+
+//finds the denominator for the pascal's triangle equation given
+  for (productloop = i; productloop > 0; productloop--)
+  {
+    denominator = denominator * productloop; 
+  }
+
+//calculates the result using the solved for numerator and denominator
+  result = numerator/denominator;
+
+  //prints out the result
+   printf("%u ", result);
+}
+printf("\n");
 
   return 0;
 }
