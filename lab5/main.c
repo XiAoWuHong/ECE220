9e241ffd46987516eac875eb79c84956f8d6acde
V Verma <vverm2@illinois.edu>
2022-03-02 18:25:54 -0600

    finished lab5

--- lab5/main.c
+++ lab5/main.c
@@ -8,4 +8,34 @@
  * Otherwise print "\n"
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "dice.h"
 
+int one, two, three;
+
+int main()
+{
+    int seed;
+    printf ("Enter Seed: \n");
+
+    scanf("%d", &seed);
+
+    srand(seed);
+
+    roll_three(&one, &two, &three);
+
+    if ((one == two) && (two == three))
+    {
+        printf("triple! \n");
+    }
+    else if (one == two || two == three || three == one)
+    {
+        printf("double! \n");
+    }
+    else 
+    {
+        printf ("\n");
+    }
+return 0;
+}
