72adf2487a143b43613cf058ff9e2352ba84b53e
V Verma <vverm2@illinois.edu>
2022-03-09 00:58:28 -0600

    I don't get why this test is failing

--- mp6/updateBoard.c
+++ mp6/updateBoard.c
@@ -13,6 +13,25 @@
  */
 
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
+    int a;  //how many living cells are there around the cell we are checking
+    int currRow = row;
+    int checkRow;   //the row in the array we are checking
+    int currCol = col;
+    int checkCol;   //the column in the array we are checking
+    for (checkRow = currRow - 1;checkRow <= currRow + 1; checkRow++){
+        if (checkRow >= 0 && checkRow <= boardRowSize){
+            for (checkCol = currCol - 1; checkCol <= currCol + 1; checkCol++){
+                if (checkCol >= 0 && checkCol <= boardColSize){
+                    if (checkCol != currCol && checkRow != currRow){
+                        if (board[checkRow * boardRowSize + checkCol] == 1){
+                            a++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return a;
 }
 /*
  * Update the game board to the next step.
@@ -24,6 +43,23 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
  * Output: board is updated with new values for next step.
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
+    int row = 0;
+    int col = 0;
+    int cell;
+
+    for (cell = 0; cell <= boardRowSize * boardColSize; cell++){
+        if (col == boardColSize){
+            row = row + 1;
+            col = 0;
+        }
+        if (row == boardRowSize && col == boardColSize){
+            break;
+        }
+        if (countLiveNeighbor(board, boardRowSize, boardColSize, row, col) == (2 || 3)){
+            board[row * boardRowSize + col] = 1;
+        }
+        col++;
+    }
 }
 
 /*
@@ -38,6 +74,23 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  * return 0 if the alive cells change for the next step.
  */ 
 int aliveStable(int* board, int boardRowSize, int boardColSize){
+    int board_copy[boardRowSize * boardColSize];
+    int b = 0;
+    int counter;
+    for (counter = 0; counter <= boardRowSize * boardColSize; counter++){
+        if (board_copy[counter] == board[counter]){
+            b++;
+        }
+        else{
+            break;
+        }
+    }
+    if (b == boardColSize * boardRowSize){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 				
