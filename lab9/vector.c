3bb606a17938e356a6cf5f285991f83d185ac525
V Verma <vverm2@illinois.edu>
2022-04-01 16:24:00 -0500

    finished lab9

--- lab9/vector.c
+++ lab9/vector.c
@@ -4,25 +4,51 @@
 
 vector_t * createVector(int initialSize)
 {
+    vector_t * new_struct = malloc(sizeof(vector_t));
+    new_struct->maxSize = initialSize;
+    new_struct->size = 0;
+    new_struct->array = malloc(new_struct->maxSize * sizeof(int *));
+
+    return new_struct;
 }
 
 void destroyVector(vector_t * vector)
 {
+    free(vector->array);
+    free(vector);
 }
 
 void resize(vector_t * vector)
 {
+    vector->maxSize = vector->maxSize * 2;
+    vector->array = realloc(vector->array, vector->maxSize * sizeof(int *));
 }
 
 void push_back(vector_t * vector, int element)
 {
+    if (vector->size == vector->maxSize){
+        resize(vector);
+    }
+    vector->array[vector->size + 1] = element;
+    vector->size = vector->size + 1;
 }
 
 int pop_back(vector_t * vector)
 {
+    if(vector->size == 0){
+        return 0;
+    }
+    int ret_val = vector->array[vector->size];
+    vector->size = vector->size - 1;
+    return ret_val;
 }
 
 int access(vector_t * vector, int index)
 {
+    if (index > vector->size - 1){
+        return 0;
+    }
+    int ret_value = vector->array[index + 1];
+    return ret_value;
 }
 
